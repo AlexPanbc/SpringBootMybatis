16004ab655cd67db67e87e816223e932b3dd4da7
AlexPanbc <1020208895pan@gmail.com>
2018-01-18 07:55:09 +0800

    Quartz 加入

---
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,74 @@
+package com.example.demo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by Alex on 2018/1/18.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+    private final static Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+    private String hostName;
+
+    private int port;
+
+    private String password;
+
+    private int timeout;
+
+    @Bean
+    public JedisPoolConfig getRedisConfig(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+
+    @Bean
+    public JedisPool getJedisPool(){
+        JedisPoolConfig config = getRedisConfig();
+        JedisPool pool = new JedisPool(config,hostName,port,timeout,password);
+        logger.info("init JredisPool ...");
+        return pool;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+}
