d20d0892081b315f86c2d742d1203838dbdafc83
AlexPanbc <panbingcan@sina.com>
2018-01-24 19:44:43 +0800

    spring boot 加入缓存jedis

---
+++ src/main/java/com/example/demo/config/EsConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Alex on 2018/1/24.
+ */
+@Configuration
+public class EsConfig {
+    @Bean
+    public TransportClient esclient(){
+
+    }
+}
